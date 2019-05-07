838f15a45b2183158306d02256f12b7395de4a3b
Dmitry Golovorushko <exalas@yandex.ru>
2019-05-07 23:48:58 +0300

    Исправлена ошибка с получением массивов категорий и лотов

--- index.php
+++ index.php
@@ -14,10 +14,7 @@ if ($con){
     $sql = "SELECT * FROM categories;";
     $result = mysqli_query($con, $sql);
     if ($result){
-        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        foreach ($rows as $row) {
-            array_push($categories, $row);
-        }
+        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
     }
 
     // Получение списка лотов
@@ -32,53 +29,9 @@ if ($con){
             ORDER BY items.start_date DESC;";
     $result = mysqli_query($con, $sql);
     if ($result){
-        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        foreach ($rows as $row) {
-            array_push($announcements, $row);
-        }
+        $announcements = mysqli_fetch_all($result, MYSQLI_ASSOC);
     }
 }
-
-/*
-$announcements = [
-    [
-        'name' => '2014 Rossignol District Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => 10900,
-        'url' => 'img/lot-1.jpg'
-    ],
-    [
-        'name' => 'DC Ply Mens 2016/2017 Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => 159999,
-        'url' => 'img/lot-2.jpg'
-    ],
-    [
-        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'category' => 'Крепления',
-        'price' => 8000,
-        'url' => 'img/lot-3.jpg'
-    ],
-    [
-        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'category' => 'Ботинки',
-        'price' => 10999,
-        'url' => 'img/lot-4.jpg'
-    ],
-    [
-        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'category' => 'Одежда',
-        'price' => 7500,
-        'url' => 'img/lot-5.jpg'
-    ],
-    [
-        'name' => 'Маска Oakley Canopy',
-        'category' => 'Разное',
-        'price' => 5400,
-        'url' => 'img/lot-6.jpg'
-    ]
-];
-*/
 
 $data_index = [
     'categories' => $categories,
